818762bbe0b42c5e45a5362abb0ad82a49014b7f
FadlyAlviansyah <fadlyalviansyah89@gmail.com>
2025-04-14 14:04:46 +0700

    set up migrations

---
+++ database/migrations/2025_04_14_012600_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('member_id')->nullable()->constrained('members');
+            $table->integer('total');
+            $table->integer('points_earned');
+            $table->integer('points_used');
+            $table->integer('amount_paid');
+            $table->integer('amount_change');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
